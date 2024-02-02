d5123448635743b39fe913a05f022c531983d839
Abhinav Pandey <158141283+abhnvv@users.noreply.github.com>
2024-02-02 16:22:13 +0530

    add healthCheck route and moved constants to constants.php

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+require_once 'Constants.php';
+
 use App\Redis\RedisClient;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -51,56 +53,62 @@ class ToDoController extends AbstractController
         $this->archiveTaskService = $archiveTaskService;
     }
 
-    #[Route('/login', name: 'app_to_do_login', methods: ['GET', 'POST'])]
+    #[Route('/', name: health_check, methods: ['GET'])]
+    public function healthCheck(): Response
+    {
+        return new Response("Server is running fine and listening to port: 8000");;
+    }
+
+    #[Route('/login', name: login, methods: ['GET', 'POST'])]
     public function login(Request $request): Response
     {
-        $sessionId = $request->cookies->get('X-Session-ID');
+        $sessionId = $request->cookies->get(SESSION_ID);
         if ($sessionId) {
-            return $this->redirectToRoute('app_to_do');
+            return $this->redirectToRoute(to_do);
         }
         if ($request->isMethod('POST')) {
-            $username = $request->request->get('username');
-            $password = $request->request->get('password');
+            $username = $request->request->get(USERNAME);
+            $password = $request->request->get(PASSWORD);
 
             $user = $this->authenticationService->authenticateUser($username, $password);
             $cookie = $this->sessionService->createSession($user->getId());
 
-            $response = $this->redirectToRoute('app_to_do');
+            $response = $this->redirectToRoute(to_do);
             $response->headers->setCookie($cookie);
             return $response;
         } else return $this->render('/user/login.html.twig');
     }
 
-    #[Route('/register', name: 'app_to_do_register', methods: ['GET', 'POST'])]
+    #[Route('/register', name: register, methods: ['GET', 'POST'])]
     public function register(Request $request): Response
     {
-        $sessionId = $request->cookies->get('X-Session-ID');
+        $sessionId = $request->cookies->get(SESSION_ID);
         if ($sessionId) {
-            return $this->redirectToRoute('app_to_do');
+            return $this->redirectToRoute(to_do);
         } else return $this->registrationService->processRegistrationForm($request);
     }
 
-    #[Route('/logout', name: 'app_to_do_logout', methods: ['GET'])]
+    #[Route('/logout', name: logout, methods: ['GET'])]
     public function logout(Request $request): Response
     {
-        $sessionId = $request->cookies->get('X-Session-ID');
+        $sessionId = $request->cookies->get(SESSION_ID);
         if (!$sessionId)
-            return $this->redirectToRoute('app_to_do_login');
-        $response = new Response();
-        $response->headers->clearCookie('X-Session-ID');
+            return $this->redirectToRoute(login);
+        $response = new Response("User has been logged out!");
+        $response->headers->clearCookie(SESSION_ID);
         $redisKey = 'sessionId:' . $sessionId;
         $this->redis->removeKey($redisKey);
         return $response;
     }
 
-    #[Route('/', name: 'app_to_do', methods: ['GET', 'POST'])]
+    #[Route('/task', name: to_do, methods: ['GET', 'POST'])]
     public function index(Request $request): Response
     {
-        $sessionId = $request->cookies->get('X-Session-ID');
+        $sessionId = $request->cookies->get(SESSION_ID);
         if (!$sessionId) {
-            return $this->redirectToRoute('app_to_do_login');
+            return $this->redirectToRoute(login);
         }
-        $this->logger->info('session id:' . $sessionId);
+
 
         $userId = SessionData::getSessionData($this->redis, $sessionId);
         $this->logger->info('user id:' . $userId);
@@ -109,25 +117,24 @@ class ToDoController extends AbstractController
 
             $archived = $request->request->get('archived', false);
 
-            // $archived = $request->query->get('archived', false); 
-            $this->logger->info('archived to do: '. $archived);
-  
+            $this->logger->info('archived to do: ' . $archived);
+
             $task = $this->getTaskService->getTask($userId, $archived);
-    
+
             return $this->render('to_do/index.html.twig', [
                 'task' => $task,
                 'archived' => $archived
             ]);
         } else {
-            return $this->redirectToRoute('app_to_do_login');
+            return $this->redirectToRoute(login);
         }
     }
 
-    #[Route('/addTask', name: 'app_to_do_add_task', methods: ['GET', 'POST'])]
+    #[Route('/task/add', name: add_task, methods: ['GET', 'POST'])]
     public function addTask(Request $request): Response
     {
-        $sessionId = $request->cookies->get('X-Session-ID');
-        $this->logger->info('session id:' . $sessionId);
+        $sessionId = $request->cookies->get(SESSION_ID);
+
         if ($sessionId) {
 
             if ($request->isMethod('POST')) {
@@ -135,45 +142,42 @@ class ToDoController extends AbstractController
             } else {
                 return $this->render('task/addTask.html.twig');
             }
-        } else return $this->redirectToRoute('app_to_do_login');
+        } else return $this->redirectToRoute(login);
     }
 
-    #[Route('/deleteTask/{taskId}', name: 'app_to_do_delete_task', methods: ['POST'])]
+    #[Route('/task/delete/{taskId}', name: delete_task, methods: ['POST'])]
     public function delete(Request $request, string $taskId): Response
     {
-        $sessionId = $request->cookies->get('X-Session-ID');
+        $sessionId = $request->cookies->get(SESSION_ID);
         if (!$sessionId) {
-            return $this->redirectToRoute('app_to_do_login');
+            return $this->redirectToRoute(login);
         }
 
-        $this->logger->info('session id:' . $sessionId);
+
         $sessionUserId = SessionData::getSessionData($this->redis, $sessionId);
 
-        $this->logger->info('session userid:' . $sessionUserId);
         if ($sessionUserId) {
             $this->logger->info('task id:' . $taskId);
             return $this->deleteTaskService->deleteTask($sessionUserId, $taskId);
         } else {
-            return $this->redirectToRoute('app_to_do_login');
+            return $this->redirectToRoute(login);
         }
     }
-    #[Route('/task/archive/{taskId}', name: 'app_to_do_archive_task', methods: ['POST'])]
+    #[Route('/task/archive/{taskId}', name: archive_task, methods: ['POST'])]
     public function archive(Request $request, string $taskId): Response
     {
-        $sessionId = $request->cookies->get('X-Session-ID');
+        $sessionId = $request->cookies->get(SESSION_ID);
         if (!$sessionId) {
-            return $this->redirectToRoute('app_to_do_login');
+            return $this->redirectToRoute(login);
         }
 
-        $this->logger->info('session id:' . $sessionId);
+
         $sessionUserId = SessionData::getSessionData($this->redis, $sessionId);
 
-        $this->logger->info('session userid:' . $sessionUserId);
         if ($sessionUserId) {
-            $this->logger->info('task id:' . $taskId);
             return $this->archiveTaskService->archiveTask($sessionUserId, $taskId);
         } else {
-            return $this->redirectToRoute('app_to_do_login');
+            return $this->redirectToRoute(login);
         }
     }
 }
